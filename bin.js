5cd36a15d1d3e843f82964e31a639cdb084fb39f
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2023-09-01 15:59:09 +0800

    update npx script

--- bin.js
+++ bin.js
@@ -3,14 +3,24 @@ const { EdgeTTS } = require('./dist/edge-tts.js')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv))
-  .usage('Usage: npx node-edge-tts <text> [options]')
+  .usage('Usage: npx node-edge-tts [options]')
   .options({
     text: { type: 'string', demandOption: true, alias: 't' },
-    path: { type: 'string', alias: 'p' }
+    filepath: { type: 'string', alias: 'f' },
+    voice: { type: 'string', alias: 'v' },
+    lang: { type: 'string', alias: 'l' },
+    outputFormat: { type: 'string', alias: 'o' },
+    saveSubtitles: { type: 'boolean', alias: 's' },
+    proxy: { type: 'string', alias: 'p' },
   })
   .default({
-    path: `./output.mp3`
+    filepath: `./output.mp3`,
+    voice: 'zh-CN-XiaoyiNeural',
+    lang: 'zh-CN',
+    outputFormat: 'audio-24khz-48kbitrate-mono-mp3',
+    saveSubtitles: false
   })
+  .example(`npx node-edge-tts -t 'Hello world' -f './output.mp3'`)
   .argv
 
 console.log(argv)
