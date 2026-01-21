d7f9a3e88a10de0316cf000f710e2286d6e5b1d9
SchneeHertz <39257008+SchneeHertz@users.noreply.github.com>
2026-01-21 21:04:08 +0800

    update to 1.2.9
    update connect user-agent

--- src/edge-tts.ts
+++ src/edge-tts.ts
@@ -61,7 +61,7 @@ class EdgeTTS {
       host: 'speech.platform.bing.com',
       origin: 'chrome-extension://jdiccldimpdaibmpdkjnbmckianbfold',
       headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/140.0.0.0 Safari/537.36 Edg/140.0.0.0',
       },
       agent: this.proxy ? new HttpsProxyAgent(this.proxy) : undefined
     })
--- test/index.js
+++ test/index.js
@@ -2,36 +2,37 @@ const { EdgeTTS } = require('../dist/edge-tts.js')
 const path = require('node:path')
 
 
-const tts = new EdgeTTS({
-  voice: 'pt-BR-ThalitaNeural',
-  lang: 'pt-BR',
-  outputFormat: 'audio-24khz-96kbitrate-mono-mp3',
-  saveSubtitles: true,
-  proxy: 'http://localhost:7890',
-  timeout: 20000
-})
-;(async () => {
-  try {
-    await tts.ttsPromise(`No Brasil, a diversidade cultural é uma das características mais marcantes. Com influências indígenas, africanas e europeias, o país desenvolveu uma mistura única de tradições e costumes. As festas populares, como o Carnaval, atraem visitantes de todo o mundo, encantados pelas cores, músicas e danças vibrantes.
-    A culinária brasileira também reflete essa diversidade. Pratos como a feijoada, a moqueca e o acarajé são apenas alguns exemplos da rica gastronomia do país, preparados com ingredientes locais que variam conforme a região.
-    Além disso, o Brasil é conhecido por suas belezas naturais. Com florestas tropicais, pantanais e um extenso litoral, oferece uma variedade de paisagens e ecossistemas. A Amazônia, muitas vezes chamada de "pulmão do mundo", destaca-se pela sua vastidão e biodiversidade.
-    Assim, explorar o Brasil é uma experiência enriquecedora, que proporciona um encontro com uma multiplicidade de culturas, sabores e paisagens.`, path.join(__dirname, 'test.mp3'))
-    console.log('end')
-  } catch (e) {
-    console.error(e)
-  }
-})()
-
 // const tts = new EdgeTTS({
+//   voice: 'pt-BR-ThalitaNeural',
+//   lang: 'pt-BR',
 //   outputFormat: 'audio-24khz-96kbitrate-mono-mp3',
 //   saveSubtitles: true,
-//   timeout: 10000
+//   proxy: 'http://localhost:7890',
+//   timeout: 20000
 // })
 // ;(async () => {
 //   try {
-//     await tts.ttsPromise(`​​我工作以后才发现，大家都是草台班子。政府草台，企业草台，我也草台，大家都草台，凑合赚钱过日子。一个企业，看着像一台奔驰在高速公路上的豪华轿车，里面其实是几个人蹬着自行车顶个壳。路上的车都是这样，大家谁都不戳破。`, path.join(__dirname, 'test.mp3'))
+//     await tts.ttsPromise(`No Brasil, a diversidade cultural é uma das características mais marcantes. Com influências indígenas, africanas e europeias, o país desenvolveu uma mistura única de tradições e costumes. As festas populares, como o Carnaval, atraem visitantes de todo o mundo, encantados pelas cores, músicas e danças vibrantes.
+//     A culinária brasileira também reflete essa diversidade. Pratos como a feijoada, a moqueca e o acarajé são apenas alguns exemplos da rica gastronomia do país, preparados com ingredientes locais que variam conforme a região.
+//     Além disso, o Brasil é conhecido por suas belezas naturais. Com florestas tropicais, pantanais e um extenso litoral, oferece uma variedade de paisagens e ecossistemas. A Amazônia, muitas vezes chamada de "pulmão do mundo", destaca-se pela sua vastidão e biodiversidade.
+//     Assim, explorar o Brasil é uma experiência enriquecedora, que proporciona um encontro com uma multiplicidade de culturas, sabores e paisagens.`, path.join(__dirname, 'test.mp3'))
 //     console.log('end')
 //   } catch (e) {
 //     console.error(e)
 //   }
 // })()
+
+const tts = new EdgeTTS({
+  outputFormat: 'audio-24khz-96kbitrate-mono-mp3',
+  saveSubtitles: true,
+  proxy: 'http://localhost:7890',
+  timeout: 10000
+})
+;(async () => {
+  try {
+    await tts.ttsPromise(`​​我工作以后才发现，大家都是草台班子。政府草台，企业草台，我也草台，大家都草台，凑合赚钱过日子。一个企业，看着像一台奔驰在高速公路上的豪华轿车，里面其实是几个人蹬着自行车顶个壳。路上的车都是这样，大家谁都不戳破。`, path.join(__dirname, 'test.mp3'))
+    console.log('end')
+  } catch (e) {
+    console.error(e)
+  }
+})()
